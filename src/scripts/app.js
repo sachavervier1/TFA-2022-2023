99e6a2e943c9c7de6165a1c3fd3d6e6c3bb4994f
Vervier Sacha <sacha.vervier@gmail.com>
2023-06-04 18:03:40 +0200

    réinisialisation pour nouveau design

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,32 +6,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 
-gsap.to("body", {
-    backgroundColor: "#FEEED5",
-    duration: 1,
-    color: "black",
 
-    scrollTrigger: {
-        trigger: ".deux",
-        start: "150px bottom",
-        toggleActions: "restart pause resume reverse "
-    },
-});
-
-
-gsap.to(".fleche--1", {
-    opacity: 0,
-    position: "absolute",
-
-    scrollTrigger: {
-        trigger: ".un",
-        start: "bottom 46%",
-        toggleActions: "restart pause resume reverse ",
-        markers: true,
-
-    },
-
-});
 
 
 
